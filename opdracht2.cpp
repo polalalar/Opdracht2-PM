054db9ad6823d05be9b37c37a50f645c4ab9bfbd
polalalar <fluppiemonster@gmail.com>
2026-09-24 14:10:04 +0200

    V2
    V2

--- opdracht2.cpp
+++ opdracht2.cpp
@@ -1,6 +1,64 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
-int main ( ) {
 
-return 0;
-}//main
+// Print een infoblokje op het scherm
+void infoblokje( ) {
+    cout << "Makers        | Jens van der Linden | Thijmen Rosenbrand"
+         << endl 
+         << "--------------------------------------------------------"
+         << endl
+         << "Studentnummer |      s5205212       |     s5225752      "
+         << endl
+         << "--------------------------------------------------------"
+         << endl
+         << "Aankomstjaar  |        2026         |       2026        "
+         << endl
+         << "--------------------------------------------------------"
+         << endl;
+    cout << "Laatste wijziging : 21-09-2026." << endl;
+    cout << "Opgave 2 - Programeermethoden - DeCoderen." 
+         << endl;
+    cout << "--------------------------------------------------------"
+         << endl;
+    cout << "Functionaliteit van programma" << endl;
+    cout << endl << endl;
+} // infoblokje
+
+int main ( ) {
+    infoblokje();
+
+    ifstream invoer ("simpelinput.txt", ios::in);
+    ofstream uitvoer ("testoutput.txt", ios::out);
+
+    char karakter = '@';
+    char vorigKarakter = '#';
+    int karakterCounter = 1;
+    karakter = invoer.get();
+
+    while (!invoer.eof()) {
+        cout << vorigKarakter << " | " << karakter << " | " << karakterCounter << endl;
+        if (vorigKarakter == '\n') {
+            uitvoer.put(vorigKarakter)
+        }
+        if (karakter == vorigKarakter) {
+            karakterCounter++;
+        } // if
+        else {
+            uitvoer.put(vorigKarakter);
+            if (karakterCounter > 1) {
+                // Geen rekening gehouden met counter >= 10
+                uitvoer.put( (char) karakterCounter);
+            } // if
+            karakterCounter = 1;
+        } // else
+        vorigKarakter = karakter;
+        karakter = invoer.get();
+    } // while
+
+    invoer.close();
+    uitvoer.close();
+
+
+    return 0;
+} // main
